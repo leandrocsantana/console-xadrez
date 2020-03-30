d169c30b73f18e81e1db8a627e3989394d0ad328
Leandro Santana <leandrocsantana@yahoo.com.br>
2020-03-30 10:22:47 -0300

    Fazendo os primeiros testes de movimentacao

--- console-xadrez/Tabuleiro/Peca.cs
+++ console-xadrez/Tabuleiro/Peca.cs
@@ -21,6 +21,9 @@ namespace tabuleiro
             this.qteMovimentos = 0;
         }
 
-    
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
--- console-xadrez/Tela.cs
+++ console-xadrez/Tela.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using tabuleiro;
+using xadrez;
 
 namespace console_xadrez
 {
@@ -28,6 +29,14 @@ namespace console_xadrez
                 Console.WriteLine();
             }
             Console.WriteLine(" a b c d e f g h");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca)
