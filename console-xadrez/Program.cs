4f0da2aae1271a7af84b2624964829107a75e1f5
Leandro Santana <leandrocsantana@yahoo.com.br>
2020-03-30 09:18:04 -0300

    git commit -m "Criando a Classe PosiçãoXadrez"

--- console-xadrez/Program.cs
+++ console-xadrez/Program.cs
@@ -8,20 +8,25 @@ namespace console_xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('b', 4);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            //try
+            //{
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+
+            //    Tela.imprimirTabuleiro(tab);
+            //}
+            //catch (TabuleiroException e) {
+            //    Console.WriteLine(e.Message);
+            //}
                 Console.ReadLine();
 
         }
